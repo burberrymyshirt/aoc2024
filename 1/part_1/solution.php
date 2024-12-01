cf58a058754cc7e2905062360634fbe0c3dcf538
Magnus Larsen <magnushlarsen01@protonmail.com>
2024-12-01 11:51:22 +0100

    solution for day 1 part 1

---
+++ 1/part_1/solution.php
@@ -0,0 +1,44 @@
+<?php
+
+function readFileData($filePath): array
+{
+    if (!file_exists($filePath)) {
+        die("File not found: $filePath\n");
+    }
+
+    $file = file_get_contents($filePath);
+    $file = str_replace("\n", ' ', $file);
+    $file = preg_replace('/\s+/', ' ', $file);
+    $file = trim($file);
+    return explode(' ', $file);
+}
+
+function main()
+{
+    // Define the input file path
+    $filePath = __DIR__ . '/data/input.txt'; // Adjust the path as needed
+
+    $explode = readFileData($filePath);
+
+    $list1 = [];
+    $list2 = [];
+
+    $size = count($explode);
+    for ($i = 0; $i < $size; $i += 2) {
+        $list1[] = (int)$explode[$i];
+        $list2[] = (int)$explode[$i + 1];
+    }
+
+    sort($list1, SORT_NUMERIC);
+    sort($list2, SORT_NUMERIC);
+
+    $distance = 0;
+    foreach ($list1 as $i => $value) {
+        $distance += abs($value - $list2[$i]);
+    }
+
+    echo "$distance\n";
+}
+
+// Run the script
+main();
